7d36c227b773f60a9b7282948a692e73c2e366cd
Soikonomou <52603162+Soikonomou@users.noreply.github.com>
2020-06-10 23:58:45 +0100

    Update infer_albert.py

--- infer_albert.py
+++ infer_albert.py
@@ -97,7 +97,7 @@ def ner_relation(text):
             order = []
             for i in range(0, len(entities)):
                 for j in range(0, len(words)):
-                    if entities[i] == words[j]:
+                    if words[j] in entities[i]:
                         order.append(i)
                         break
             ordered_entities = [x for _, x in sorted(zip(order, entities))]
