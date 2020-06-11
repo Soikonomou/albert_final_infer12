f966eac7e86193c55131d460c5cab26e2df344c4
Soikonomou <52603162+Soikonomou@users.noreply.github.com>
2020-06-11 09:00:45 +0100

    Update infer_albert.py

--- infer_albert.py
+++ infer_albert.py
@@ -17,37 +17,29 @@ This fine-tunes the BERT model on SemEval task
 logging.basicConfig(format='%(asctime)s [%(levelname)s]: %(message)s',
                     datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO)
 logger = logging.getLogger('__file__')
+parser = ArgumentParser()
+parser.add_argument("--task", type=str, default='semeval', help='semeval, fewrel')
+parser.add_argument("--train_data", type=str, default='./data/SemEval2010_task8_all_data/SemEval2010_task8_training/TRAIN_FILE.TXT',help="training data .txt file path")
+parser.add_argument("--test_data", type=str, default='./data/SemEval2010_task8_all_data/SemEval2010_task8_testing_keys/TEST_FILE_FULL.TXT',help="test data .txt file path")
+parser.add_argument("--use_pretrained_blanks", type=int, default=0,help="0: Don't use pre-trained blanks model, 1: use pre-trained blanks model")
+parser.add_argument("--num_classes", type=int, default=19, help='number of relation classes')
+parser.add_argument("--batch_size", type=int, default=32, help="Training batch size")
+parser.add_argument("--gradient_acc_steps", type=int, default=1,help="No. of steps of gradient accumulation")
+parser.add_argument("--max_norm", type=float, default=1.0, help="Clipped gradient norm")
+# mixed precision doesn't seem to train well
+parser.add_argument("--fp16", type=int, default=0,help="1: use mixed precision ; 0: use floating point 32")
+parser.add_argument("--num_epochs", type=int, default=20, help="No of epochs")
+parser.add_argument("--lr", type=float, default=0.00005, help="learning rate")
+parser.add_argument("--model_no", type=int, default=1, help='''Model ID: 0 - BERT\n1 - ALBERT''')
+parser.add_argument("--train", type=int, default=0, help="0: Don't train, 1: train")
+parser.add_argument("--infer", type=int, default=1, help="0: Don't infer, 1: Infer")
 
+args = parser.parse_args()
+
+if args.train == 1:
+  net = train_and_fit(args)
 
 def ner_relation(text):
-    parser = ArgumentParser()
-    parser.add_argument("--task", type=str, default='semeval', help='semeval, fewrel')
-    parser.add_argument("--train_data", type=str, default='./data/SemEval2010_task8_all_data/SemEval2010_task8_training/TRAIN_FILE.TXT',
-                        help="training data .txt file path")
-    parser.add_argument("--test_data", type=str, default='./data/SemEval2010_task8_all_data/SemEval2010_task8_testing_keys/TEST_FILE_FULL.TXT',
-                        help="test data .txt file path")
-    parser.add_argument("--use_pretrained_blanks", type=int, default=0,
-                        help="0: Don't use pre-trained blanks model, 1: use pre-trained blanks model")
-    parser.add_argument("--num_classes", type=int, default=19, help='number of relation classes')
-    parser.add_argument("--batch_size", type=int, default=32, help="Training batch size")
-    parser.add_argument("--gradient_acc_steps", type=int, default=1,
-                        help="No. of steps of gradient accumulation")
-    parser.add_argument("--max_norm", type=float, default=1.0, help="Clipped gradient norm")
-    # mixed precision doesn't seem to train well
-    parser.add_argument("--fp16", type=int, default=0,
-                        help="1: use mixed precision ; 0: use floating point 32")
-    parser.add_argument("--num_epochs", type=int, default=20, help="No of epochs")
-    parser.add_argument("--lr", type=float, default=0.00005, help="learning rate")
-    parser.add_argument("--model_no", type=int, default=1, help='''Model ID: 0 - BERT\n
-                                                                            1 - ALBERT''')
-    parser.add_argument("--train", type=int, default=0, help="0: Don't train, 1: train")
-    parser.add_argument("--infer", type=int, default=1, help="0: Don't infer, 1: Infer")
-
-    args = parser.parse_args()
-
-    if args.train == 1:
-        net = train_and_fit(args)
-
     if args.infer == 1:
         inferer = infer_from_trained(args, detect_entities=False)
         body = text
@@ -98,7 +90,7 @@ def ner_relation(text):
             for i in range(0, len(entities)):
                 for j in range(0, len(words)):
                     if words[j] in entities[i]:
-                        order.append(i)
+                        order.append(j)
                         break
             ordered_entities = [x for _, x in sorted(zip(order, entities))]
             for previous, current in zip(ordered_entities, ordered_entities[1:]):
